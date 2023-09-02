74383669acdc71b7f59ef795ca4849eed9adad23
karneges <karneges2012@gmail.com>
2023-09-03 03:06:22 +0900

    add deploy script

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,7 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import * as dotenv from "dotenv";
+dotenv.config();
 const config: HardhatUserConfig = {
+  networks: {
+    zkevm: {
+      url: `https://rpc.public.zkevm-test.net`,
+      accounts: [process.env.PRIVATE_KEY_MUMBAI!],
+    },
+  },
   solidity: "0.8.19",
 };
 
