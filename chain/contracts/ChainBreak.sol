23b29c51dd143e43fc74e260ab94df9bbbd41aaf
Maga <steam.30mb1@gmail.com>
2023-09-02 23:19:56 +0400

    Better view methods

--- chain/contracts/ChainBreak.sol
+++ chain/contracts/ChainBreak.sol
@@ -2,51 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./Utility.sol";
+import "./interfaces/IChainBreak.sol";
 
-contract ChainBreak {
-    event Transaction(address user1, address user2, Tx transaction, uint idx);
-    event TransactionConfirmed(address user1, address user2, Tx transaction, uint idx);
-    event TransactionRejected(address user1, address user2, Tx transaction, uint idx);
-
-
-    enum TxStatus {CreatedBy1, CreatedBy2, Confirmed, Rejected}
-    enum TxType {Regular, Auto}
-
-    struct Tx {
-        int amount;
-        string description;
-        uint32 createdAt;
-        bool from1; // true: 1 -> 2, false: 2 -> 1
-        TxStatus status;
-        TxType txType;
-    }
-
-    struct Channel {
-        int balance1;
-        int balance2;
-        uint fees;
-
-        Tx[] txs;
-    }
-
-    struct ChannelView {
-        address user1;
-        address user2;
-        Channel channel;
-    }
-
-    struct UserView {
-        address user;
-        ChannelView[] views;
-    }
-
-    struct TxView {
-        address user1;
-        address user2;
-        uint idx;
-        Tx tx;
-    }
-
+contract ChainBreak is IChainBreak {
     // all addresses user has _channels with
     mapping (address => address[]) private _userContacts;
     mapping (address => mapping (address => Channel)) private _channels;
