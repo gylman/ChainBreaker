e7cddf3b3f6158602b29e4da2e2a45767d25edef
karneges <karneges2012@gmail.com>
2023-09-02 23:11:09 +0900

    liquidation test

--- chain/test/Lock.ts
+++ chain/test/Lock.ts
@@ -2,10 +2,22 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { ChainBreak } from "../typechain-types";
 import { getFirstUserAddress } from "./utils";
+type HHSigner = (ReturnType<typeof ethers.getSigners> extends Promise<infer T>
+  ? T
+  : never)[0];
 let contract: ChainBreak;
+let deployer: HHSigner;
+let user1: HHSigner;
+let user2: HHSigner;
+let user3: HHSigner;
+
 describe("ChainBreak", function () {
   it("deploy ChainBreak", async () => {
-    const [deployer, user1, user2, user3] = await ethers.getSigners();
+    const [d, u1, u2, u3] = await ethers.getSigners();
+    deployer = d;
+    user1 = u1;
+    user2 = u2;
+    user3 = u3;
 
     contract = await ethers.deployContract("ChainBreak", []);
     await contract
@@ -15,8 +27,6 @@ describe("ChainBreak", function () {
   });
 
   it("create and confirm tx", async () => {
-    const [deployer, user1, user2, user3] = await ethers.getSigners();
-
     const firstUser = getFirstUserAddress([user1.address, user2.address]);
 
     const data = await contract.channelFor(user1, user2);
@@ -37,9 +47,29 @@ describe("ChainBreak", function () {
       expect(data.fees).to.equal(0);
       expect(data.txs.length).to.equal(1);
       const firstTx = data.txs[0];
-      expect(firstTx.amount).to.equal(100);
-      expect(firstTx.description).to.equal("test");
       expect(firstTx.status).to.equal(2);
     }
+  });
+
+  it("create second channel", async () => {
+    await contract.connect(user2).createTx(user3, 200, "test", true);
+
+    await contract.connect(user3).confirmTx(user2, 0);
+    const data = await contract.channelFor(user2, user3);
+    expect(data.balance1).to.equal(200);
+    expect(data.balance2).to.equal(-200);
+  });
+  it("create channel between 1 and 3 user", async () => {
+    await contract.connect(user1).createTx(user3, 300, "test", true);
+    await contract.connect(user3).confirmTx(user1, 0);
+    const data = await contract.channelFor(user1, user3);
+    expect(data.balance1).to.equal(300);
+    expect(data.balance2).to.equal(-300);
+  });
+
+  it("try to liquidate", async () => {
+    await contract
+      .connect(deployer)
+      .breakDebtCircuit([user1, user2, user3, user1], 100);
   });
 });
--- chain/test/utils/index.ts
+++ chain/test/utils/index.ts
@@ -3,3 +3,4 @@ import { ethers, toNumber } from "ethers";
 export const getFirstUserAddress = (addresses: Array<string>) => {
   return addresses.sort()[0];
 };
+export const createAndConfirmTx = () => {};
