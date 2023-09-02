a69065e1a0d0eecf943ed162ef53c2dff1b0cbc9
karneges <karneges2012@gmail.com>
2023-09-03 01:00:57 +0900

    add liquidation tests

--- chain/test/Lock.ts
+++ chain/test/Lock.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { ChainBreak } from "../typechain-types";
 import { getFirstUserAddress } from "./utils";
+import _ from "lodash";
 type HHSigner = (ReturnType<typeof ethers.getSigners> extends Promise<infer T>
   ? T
   : never)[0];
@@ -60,16 +61,62 @@ describe("ChainBreak", function () {
     expect(data.balance2).to.equal(-200);
   });
   it("create channel between 1 and 3 user", async () => {
-    await contract.connect(user1).createTx(user3, 300, "test", true);
-    await contract.connect(user3).confirmTx(user1, 0);
+    await contract.connect(user3).createTx(user1, 300, "test", true);
+    await contract.connect(user1).confirmTx(user3, 0);
     const data = await contract.channelFor(user1, user3);
-    expect(data.balance1).to.equal(300);
-    expect(data.balance2).to.equal(-300);
+    expect(data.balance1).to.equal(-300);
+    expect(data.balance2).to.equal(300);
   });
 
   it("try to liquidate", async () => {
+    const semiRoute = await Promise.all(
+      [
+        [user1, user2],
+        [user3, user1],
+        [user2, user3],
+      ].map(async ([u1, u2]) => {
+        const users = await contract.sort(u1, u2);
+        const balances = await contract
+          .channelFor(u1, u2)
+          .then(({ balance1, balance2 }) => [balance1, balance2]);
+
+        return _.zip(users, balances)
+          .map(([user, balance]) => ({
+            user,
+            balance,
+          }))
+          .sort((a, b) => Number(b.balance) - Number(a.balance));
+      }),
+    )
+      .then((pairs) => pairs.flatMap((pair) => pair))
+      .then((flatPairs) =>
+        _(flatPairs)
+          .uniqBy((el) => el.user)
+          .reverse()
+          .value(),
+      );
+    const route = [...semiRoute, semiRoute[0]];
     await contract
       .connect(deployer)
-      .breakDebtCircuit([user1, user2, user3, user1], 100);
+      // .breakDebtCircuit([user1, user3, user2, user1], 100);
+      .breakDebtCircuit(
+        route.map((el) => el.user!),
+        100,
+      );
+    {
+      const data = await contract.channelFor(user1, user2);
+      expect(data.balance1).to.equal(0);
+      expect(data.balance2).to.equal(0);
+    }
+    {
+      const data = await contract.channelFor(user2, user3);
+      expect(data.balance1).to.equal(100);
+      expect(data.balance2).to.equal(-100);
+    }
+    {
+      const data = await contract.channelFor(user3, user1);
+      expect(data.balance1).to.equal(-200);
+      expect(data.balance2).to.equal(200);
+    }
   });
 });
