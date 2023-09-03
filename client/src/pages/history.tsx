1390d2413291ef149f196266b812ff765e75e825
Keonwoo Kim <me@kanu.kim>
2023-09-03 10:37:21 +0900

    fix: ??????

--- client/src/pages/history.tsx
+++ client/src/pages/history.tsx
@@ -45,28 +45,11 @@ export default function History() {
           <div className="absolute bottom-[5.5rem] left-1/2 w-11/12 max-w-md -translate-x-1/2 rounded-2xl border-2 border-gray-800 bg-white px-5 py-3 shadow-[6px_6px_0px_rgb(31_41_55)]">
             {(() => {
               const total = transactions.value.reduce((acc, tx) => acc + Number(tx.amount) * (tx.isSpent ? -1 : 1), 0);
-              return `You ${total >= 0 ? "lent" : "owe"} \$${Math.abs(total)}.`;
+              return `You ${total >= 0 ? "owe" : "lent"} \$${Math.abs(total)}.`;
             })()}
           </div>
         </Tabs.Content>
         <Tabs.Content className="min-h-full space-y-6 p-6 pb-[10rem]" value="withdrawals">
-          <ul className="flex flex-col-reverse gap-2">
-            {transactions.value
-              .filter((tx) => !tx.isSpent)
-              .map((tx) => (
-                <Item key={`${tx.address}:${tx.idx}`} tx={tx} />
-              ))}
-          </ul>
-          <div className="absolute bottom-[5.5rem] left-1/2 w-11/12 max-w-md -translate-x-1/2 rounded-2xl border-2 border-gray-800 bg-white px-5 py-3 shadow-[6px_6px_0px_rgb(31_41_55)]">
-            {(() => {
-              const total = transactions.value
-                .filter((tx) => !tx.isSpent)
-                .reduce((acc, tx) => acc + Number(tx.amount), 0);
-              return `You lent \$${total}.`;
-            })()}
-          </div>
-        </Tabs.Content>
-        <Tabs.Content className="min-h-full space-y-6 p-6 pb-[10rem]" value="deposits">
           <ul className="flex flex-col-reverse gap-2">
             {transactions.value
               .filter((tx) => tx.isSpent)
@@ -78,6 +61,23 @@ export default function History() {
             {(() => {
               const total = transactions.value
                 .filter((tx) => tx.isSpent)
+                .reduce((acc, tx) => acc + Number(tx.amount), 0);
+              return `You lent \$${total}.`;
+            })()}
+          </div>
+        </Tabs.Content>
+        <Tabs.Content className="min-h-full space-y-6 p-6 pb-[10rem]" value="deposits">
+          <ul className="flex flex-col-reverse gap-2">
+            {transactions.value
+              .filter((tx) => !tx.isSpent)
+              .map((tx) => (
+                <Item key={`${tx.address}:${tx.idx}`} tx={tx} />
+              ))}
+          </ul>
+          <div className="absolute bottom-[5.5rem] left-1/2 w-11/12 max-w-md -translate-x-1/2 rounded-2xl border-2 border-gray-800 bg-white px-5 py-3 shadow-[6px_6px_0px_rgb(31_41_55)]">
+            {(() => {
+              const total = transactions.value
+                .filter((tx) => !tx.isSpent)
                 .reduce((acc, tx) => acc + Number(tx.amount), 0);
               return `You owe \$${total}.`;
             })()}
@@ -97,7 +97,7 @@ function Item({ tx }: { tx: Tx }) {
       key={tx.address}
       className={cx(
         "justify-between rounded-xl border-2 border-gray-800 px-4 py-2",
-        tx.status.startsWith("pending") ? "bg-orange-100" : !tx.isSpent ? "bg-red-100" : "bg-lime-100",
+        tx.status.startsWith("pending") ? "bg-orange-100" : !tx.isSpent ? "bg-lime-100" : "bg-red-100",
       )}
     >
       {/* name */}
