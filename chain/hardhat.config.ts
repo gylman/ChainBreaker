bb2f5f28aec64cc593c8c374959b38140db24d19
karneges <karneges2012@gmail.com>
2023-09-03 10:57:19 +0900

    update hardhat config

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -3,20 +3,20 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 dotenv.config();
 const config: HardhatUserConfig = {
-  // networks: {
-  //   zkevm: {
-  //     url: "https://rpc.public.zkevm-test.net",
-  //     accounts: [process.env.PRIVATE_KEY_MUMBAI!],
-  //   },
-  //   linea: {
-  //     url: "https://rpc.goerli.linea.build",
-  //     accounts: [process.env.PRIVATE_KEY_MUMBAI!],
-  //   },
-  //   taiko: {
-  //     url: "https://rpc.test.taiko.xyz",
-  //     accounts: [process.env.PRIVATE_KEY_MUMBAI!],
-  //   },
-  // },
+  networks: {
+    zkevm: {
+      url: "https://rpc.public.zkevm-test.net",
+      accounts: [process.env.PRIVATE_KEY_MUMBAI!],
+    },
+    linea: {
+      url: "https://rpc.goerli.linea.build",
+      accounts: [process.env.PRIVATE_KEY_MUMBAI!],
+    },
+    taiko: {
+      url: "https://rpc.test.taiko.xyz",
+      accounts: [process.env.PRIVATE_KEY_MUMBAI!],
+    },
+  },
   solidity: {
     version: "0.8.19",
     settings: {
